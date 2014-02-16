2a3dd4f640e59f03010da8823529f4a569216b5a
Danny Tuppeny <danny@tuppeny.com>
2014-02-16 14:24:35 +0000

    Initial commit of empty shared library

---
+++ DanTup.TestAdapters/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("DanTup.TestAdapters")]
+[assembly: AssemblyProduct("DanTup.TestAdapters")]
---
+++ DanTup.TestAdapters/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Danny Tuppeny")]
+[assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
