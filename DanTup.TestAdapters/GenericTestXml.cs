2aa0f6fb90a01544d14ec3a8ca57b8b8c794d6b9
Danny Tuppeny <danny@tuppeny.com>
2014-02-16 16:36:56 +0000

    Data class for holding test cases/results

---
+++ DanTup.TestAdapters/GenericTestXml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Serialization;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+namespace DanTup.TestAdapters
+{
+	[XmlRoot("Tests")]
+	[Serializable]
+	public class GenericTests
+	{
+		[XmlElement("Test")]
+		public GenericTest[] Tests { get; set; }
+	}
+
+	[Serializable]
+	public class GenericTest
+	{
+		public string Name { get; set; }
+		public string DisplayName { get; set; }
+		public string CodeFilePath { get; set; }
+		public int LineNumber { get; set; }
+
+		public TestOutcome Outcome { get; set; }
+		public string ErrorMessage { get; set; }
+		public string ErrorStackTrace { get; set; }
+	}
+}
