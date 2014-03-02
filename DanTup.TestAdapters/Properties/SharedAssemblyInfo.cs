399c7cee95c57e0c601a85e0578ce41150e7c9c8
Danny Tuppeny <danny@tuppeny.com>
2014-03-02 17:47:23 +0000

    Bump version to 1.5.

--- DanTup.TestAdapters/Properties/SharedAssemblyInfo.cs
+++ DanTup.TestAdapters/Properties/SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Danny Tuppeny")]
 [assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.4.*")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.*")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
