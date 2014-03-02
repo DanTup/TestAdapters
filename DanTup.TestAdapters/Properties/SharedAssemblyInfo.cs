946a16307d5d7fd2cdec2f8bee78d12b327cac7f
Danny Tuppeny <danny@tuppeny.com>
2014-03-02 15:13:47 +0000

    Bump version numbers to 1.3
    Why can't I have these in one place?! :-(

--- DanTup.TestAdapters/Properties/SharedAssemblyInfo.cs
+++ DanTup.TestAdapters/Properties/SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Danny Tuppeny")]
 [assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.2.*")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
