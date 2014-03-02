baf16007f73556508b57fff8639f8f97662c625e
Danny Tuppeny <danny@tuppeny.com>
2014-03-02 19:28:00 +0000

    Bumped to version 1.6

--- DanTup.TestAdapters/Properties/SharedAssemblyInfo.cs
+++ DanTup.TestAdapters/Properties/SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Danny Tuppeny")]
 [assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.5.*")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.6.*")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
