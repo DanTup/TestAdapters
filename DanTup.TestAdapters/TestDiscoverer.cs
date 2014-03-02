36f0ce90f10d95f3de01c646682750bcd7407397
Danny Tuppeny <danny@tuppeny.com>
2014-03-02 17:38:51 +0000

    When listing tests, if we have stack traces but no location, attempt to parse them
    This is part of Issue #4 SUPERBODGE

--- DanTup.TestAdapters/TestDiscoverer.cs
+++ DanTup.TestAdapters/TestDiscoverer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
@@ -10,6 +11,11 @@ namespace DanTup.TestAdapters
 {
 	public abstract class TestDiscoverer : ITestDiscoverer
 	{
+		/// <summary>
+		/// Location that the extension is installed; used for locating resources that were included (eg. lua.exe).
+		/// </summary>
+		public abstract string ExtensionFolder { get; }
+
 		protected abstract ExternalTestExecutor ExternalTestExecutor { get; }
 		protected abstract Uri ExecutorUri { get; }
 
@@ -30,13 +36,40 @@ namespace DanTup.TestAdapters
 			var directory = Path.GetDirectoryName(source);
 			// VS will open the file, but not jump to the line number if the path contais "\.\", so make sure we handle this
 			var path = test.CodeFilePath != null ? Path.Combine(directory, test.CodeFilePath.StartsWith(".\\") ? test.CodeFilePath.Substring(2) : test.CodeFilePath) : null;
+			var line = test.LineNumber;
+
+			// SUPERBODGE (See Issue #4)
+			// If we don't have the location of the test, but we have a stack, parse it out
+			// This is temporary until we can find a better way to get filename/line numbers via Jasmine
+			if (path == null && !string.IsNullOrWhiteSpace(test.ErrorStackTrace))
+				TryParseLocationFromStack(test, out path, out line);
 
 			return new TestCase(test.Name, ExecutorUri, source)
 			{
 				DisplayName = test.DisplayName,
 				CodeFilePath = path,
-				LineNumber = test.LineNumber
+				LineNumber = line
 			};
+		}
+
+		private void TryParseLocationFromStack(GenericTest test, out string path, out int line)
+		{
+			// First line is the error message
+			// Then there's one line of our framework; but just to be a bit more future-proof, exclude any that look like extension
+			var matchingLine = test.ErrorStackTrace.Split('\n').Skip(1).FirstOrDefault(l => !l.ToLower().Contains(ExtensionFolder.ToLower()));
+
+			var match = Regex.Match(matchingLine, @"\((.+):(\d+):\d+\)");
+
+			if (match.Groups.Count >= 3)
+			{
+				path = match.Groups[1].Value;
+				line = int.Parse(match.Groups[2].Value);
+			}
+			else
+			{
+				path = null;
+				line = 0;
+			}
 		}
 	}
 }
