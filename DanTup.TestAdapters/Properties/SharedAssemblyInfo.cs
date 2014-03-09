2ff1c5a2d619bc23f73caeecd7d78c5ff0a9123e
Danny Tuppeny <danny@tuppeny.com>
2014-03-09 18:19:20 +0000

    Revert "Bump versions to 1.9"
    This reverts commit 35a429fe5a0a5acfdb57fe20e10b04deb98ffb44.

--- DanTup.TestAdapters/Properties/SharedAssemblyInfo.cs
+++ DanTup.TestAdapters/Properties/SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Danny Tuppeny")]
 [assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.9.*")]
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyVersion("1.8.*")]
+[assembly: AssemblyFileVersion("1.8.0.0")]
