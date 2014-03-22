3735a74b5559798228f5f446cdd4f73e1db2c530
Danny Tuppeny <danny@tuppeny.com>
2014-03-22 12:25:59 +0000

    Bump all vsix versions to 1.9 using UpdateVersions.fsx

--- DanTup.TestAdapters.Xml.Vsix/XmlPackage.cs
+++ DanTup.TestAdapters.Xml.Vsix/XmlPackage.cs
@@ -2,7 +2,7 @@
 
 namespace DanTup.TestAdapters.Xml.Vsix
 {
-	[InstalledProductRegistration("DanTup's Xml Test Result Parser (Karma/Jasmine/Angular)", @"Test Adapter thread reads static XML files, for use with external tools such as Karma/Jasmine/Angular testing. https://github.com/DanTup/TestAdapters", "1.8")]
+	[InstalledProductRegistration("DanTup's Xml Test Result Parser (Karma/Jasmine/Angular)", @"Test Adapter thread reads static XML files, for use with external tools such as Karma/Jasmine/Angular testing. https://github.com/DanTup/TestAdapters", "1.9")]
 	public sealed class XmlPackage
 	{
 	}
